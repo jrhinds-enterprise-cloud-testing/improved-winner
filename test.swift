23e84d9b5946d2e4a30d0131824c30b73f8b8807
Jonathan R. Hinds <98826425+jrhinds@users.noreply.github.com>
2025-10-15 14:59:05 -0500

    Add simple Hello World test in Swift

---
+++ test.swift
@@ -0,0 +1,12 @@
+// Simple Hello World test in Swift
+
+func helloWorld() -> String {
+    return "Hello, world!"
+}
+
+// Test
+if helloWorld() == "Hello, world!" {
+    print("Test passed!")
+} else {
+    print("Test failed!")
+}
