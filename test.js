bdbae8fc476bb59aebef2219ae65d9ce2e0ca650
Jonathan R. Hinds <98826425+jrhinds@users.noreply.github.com>
2025-10-15 14:58:29 -0500

    Add simple Hello World test in JavaScript

---
+++ test.js
@@ -0,0 +1,12 @@
+// Simple Hello World test in JavaScript
+
+function helloWorld() {
+  return "Hello, world!";
+}
+
+// Test
+if (helloWorld() === "Hello, world!") {
+  console.log("Test passed!");
+} else {
+  console.log("Test failed!");
+}
